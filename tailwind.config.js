a6e690fd5aa5b77bdbdad0b58904f6f55fd91612
megiserjanaj <megiserjanaj@gmail.com>
2021-11-09 16:53:07 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
      darkMode: false,
      theme: {
-       extend: {},
+       extend: {
+           animation: {
+               'bounceIn': '0.5s'
+           }
+       },
        colors: {
         white: {
             50: '#ffffff',
